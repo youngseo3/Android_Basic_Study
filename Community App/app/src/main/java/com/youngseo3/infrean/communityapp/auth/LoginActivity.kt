f671c4a9452e951ee7265d6d94af7c40206037a6
youngseo3 <wys4475@gmail.com>
2023-12-26 19:56:23 +0900

    회원가입 및 로그인 레이아웃 구성

---
+++ Community App/app/src/main/java/com/youngseo3/infrean/communityapp/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.youngseo3.infrean.communityapp.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.youngseo3.infrean.communityapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
