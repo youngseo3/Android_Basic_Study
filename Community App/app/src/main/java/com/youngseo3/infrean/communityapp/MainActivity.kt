74fa5ac6760e310ec6653ce03590334dac95c7ec
youngseo3 <wys4475@gmail.com>
2024-01-01 15:57:19 +0900

    Firebase 이메일, 익명 로그인 구현

--- Community App/app/src/main/java/com/youngseo3/infrean/communityapp/MainActivity.kt
+++ Community App/app/src/main/java/com/youngseo3/infrean/communityapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.youngseo3.infrean.communityapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.youngseo3.infrean.communityapp.auth.IntroActivity
+import com.youngseo3.infrean.communityapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.logoutBtn.setOnClickListener {
+
+            auth.signOut()
+
+            val intent = Intent(this, IntroActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
 }
--- Community App/app/src/main/java/com/youngseo3/infrean/communityapp/auth/IntroActivity.kt
+++ Community App/app/src/main/java/com/youngseo3/infrean/communityapp/auth/IntroActivity.kt
@@ -3,17 +3,24 @@ package com.youngseo3.infrean.communityapp.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.youngseo3.infrean.communityapp.MainActivity
 import com.youngseo3.infrean.communityapp.R
 import com.youngseo3.infrean.communityapp.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_intro)
-
+        auth = Firebase.auth
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
 
         binding.loginBtn.setOnClickListener {
@@ -24,6 +31,23 @@ class IntroActivity : AppCompatActivity() {
         binding.joinBtn.setOnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.noAccountBtn.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+
+                    } else {
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+                    }
+                }
         }
     }
 }
