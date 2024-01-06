f99139baf0805f9b1956373772e5a66f55108e21
youngseo3 <wys4475@gmail.com>
2024-01-06 17:42:46 +0900

    네비게이션 뷰 구성

--- Community App/app/src/main/java/com/youngseo3/infrean/communityapp/MainActivity.kt
+++ Community App/app/src/main/java/com/youngseo3/infrean/communityapp/MainActivity.kt
@@ -10,6 +10,7 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.youngseo3.infrean.communityapp.auth.IntroActivity
 import com.youngseo3.infrean.communityapp.databinding.ActivityMainBinding
+import com.youngseo3.infrean.communityapp.fragments.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,13 +22,53 @@ class MainActivity : AppCompatActivity() {
         auth = Firebase.auth
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.logoutBtn.setOnClickListener {
+//        binding.logoutBtn.setOnClickListener {
+//
+//            auth.signOut()
+//
+//            val intent = Intent(this, IntroActivity::class.java)
+//            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//            startActivity(intent)
+//        }
 
-            auth.signOut()
+        initBottomNavigationView()
+    }
 
-            val intent = Intent(this, IntroActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
+    private fun initBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_container, HomeFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_goodtip -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_container, TipFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_talk -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_container, TalkFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_bookmark -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_container, BookmarkFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_store -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_container, StoreFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
--- Community App/app/src/main/java/com/youngseo3/infrean/communityapp/SplashActivity.kt
+++ Community App/app/src/main/java/com/youngseo3/infrean/communityapp/SplashActivity.kt
@@ -4,16 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.youngseo3.infrean.communityapp.auth.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 3000)
+        auth = Firebase.auth
+
+        if(auth.currentUser?.uid == null) {
+            Log.d("SplashActivity", "null")
+
+            Handler().postDelayed({
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            }, 3000)
+
+        } else {
+            Log.d("SplashActivity", "not null")
+
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }, 3000)
+
+        }
+
     }
 }
