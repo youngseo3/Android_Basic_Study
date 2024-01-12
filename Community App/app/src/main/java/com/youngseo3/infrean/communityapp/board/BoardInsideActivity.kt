7c3d18f017db3c7e406c533b534289a0c4fa0243
youngseo3 <wys4475@gmail.com>
2024-01-12 21:56:21 +0900

    게시글 이미지 업로드 및 다운로드

--- Community App/app/src/main/java/com/youngseo3/infrean/communityapp/board/BoardInsideActivity.kt
+++ Community App/app/src/main/java/com/youngseo3/infrean/communityapp/board/BoardInsideActivity.kt
@@ -3,12 +3,16 @@ package com.youngseo3.infrean.communityapp.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import com.bumptech.glide.disklrucache.DiskLruCache.Value
+import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.youngseo3.infrean.communityapp.R
 import com.youngseo3.infrean.communityapp.databinding.ActivityBoardInsideBinding
 import com.youngseo3.infrean.communityapp.utils.FBRef
@@ -24,7 +28,7 @@ class BoardInsideActivity : AppCompatActivity() {
         // 1.
         val key = intent.getStringExtra("key").toString()
         getBoardData(key)
-
+        getImageData(key)
         // 2.
 //        val bundle = intent.getBundleExtra("board")
 //        val dataModel = bundle?.getSerializable("board") as BoardModel
@@ -55,5 +59,24 @@ class BoardInsideActivity : AppCompatActivity() {
         }
 
         FBRef.boardRef.child(key).addValueEventListener(postListener)
+    }
+
+    private fun getImageData(key: String) {
+        // Reference to an image file in Cloud Storage
+        val storageReference = Firebase.storage.reference.child("$key.png")
+
+// ImageView in your Activity
+        val imageView = binding.getImageArea
+
+        storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task ->
+            if(task.isSuccessful) {
+
+                Glide.with(this)
+                    .load(task.result)
+                    .into(imageView)
+            } else {
+
+            }
+        })
     }
 }
--- Community App/app/src/main/java/com/youngseo3/infrean/communityapp/board/BoardWriteActivity.kt
+++ Community App/app/src/main/java/com/youngseo3/infrean/communityapp/board/BoardWriteActivity.kt
@@ -1,18 +1,25 @@
 package com.youngseo3.infrean.communityapp.board
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.youngseo3.infrean.communityapp.R
 import com.youngseo3.infrean.communityapp.databinding.ActivityBoardWriteBinding
 import com.youngseo3.infrean.communityapp.utils.FBAuth
 import com.youngseo3.infrean.communityapp.utils.FBRef
+import java.io.ByteArrayOutputStream
 
 class BoardWriteActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBoardWriteBinding
-
+    private var isImageUpload = false
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -23,13 +30,56 @@ class BoardWriteActivity : AppCompatActivity() {
             val content = binding.edittextBoardwriteContent.text.toString()
             val uid = FBAuth.getUid()
             val time = FBAuth.getTime()
+
+            /* 이미지 이름에 대한 정보를 모르기 때문에 이미지의 이름을
+               key값으로 해줘서 이미지에 대한 정보를 찾기 쉽게 함 */
+            val key = FBRef.boardRef.push().key.toString()
+
             FBRef.boardRef
-                .push()
+                .child(key)
                 .setValue(BoardModel(title, content, uid, time))
+
+            if(isImageUpload) imageUpload(key)
+
 
             Toast.makeText(this, "게시글 입력 완료", Toast.LENGTH_SHORT).show()
             finish()
         }
 
+        // 핸드폰에서 사진 선택
+        binding.imageAreaIv.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, 100)
+            isImageUpload = true
+        }
+    }
+    private fun imageUpload(key: String) { // 이미지 업로드
+        // Get the data from an ImageView as bytes
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+        val mountainsRef = storageRef.child("$key.png")
+
+        val imageView = binding.imageAreaIv
+
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        var uploadTask = mountainsRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+            // Handle unsuccessful uploads
+        }.addOnSuccessListener { taskSnapshot ->
+            // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+            // ...
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == 100) {
+            binding.imageAreaIv.setImageURI(data?.data)
+        }
     }
 }
